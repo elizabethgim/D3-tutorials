0db386e85a4e403cc21079e54b49756922a1735c
elizabethgim <yujingim@naver.com>
2025-11-18 20:30:47 +0900

    add: change to scatter plot

--- index.js
+++ index.js
@@ -6,32 +6,36 @@ const height = +svg.attr('height');
 const render = data => {
   const xValue = d => d.population;
   const yValue = d => d.country;
-  const margin = {top:50, right:30, bottom:77, left:180};
+  const margin = {top:50, right:40, bottom:77, left:180};
   const innerWidth = width - margin.left - margin.right;
   const innerHeight = height - margin.top - margin.bottom;
 
   const xScale = d3.scaleLinear()
     .domain([0, d3.max(data, xValue)])
-    .range([0, innerWidth]);
+    .range([0, innerWidth])
+    .nice();
 
-  const yScale = d3.scaleBand()
+  const yScale = d3.scalePoint()
       .domain(data.map(yValue))
       .range([0, innerHeight])
-      .padding(0.1);
+      .padding(0.7);
 
   const g = svg.append("g")
     .attr('transform', `translate(${margin.left}, ${margin.top})`);
   
-    const xAxisTickFormat = number =>
-      d3.format('.3s')(number)
-      .replace('G', 'B');
+  const xAxisTickFormat = number =>
+    d3.format('.3s')(number)
+    .replace('G', 'B');
 
-    const xAxis = d3.axisBottom(xScale)
-      .tickFormat(xAxisTickFormat)
-      .tickSize(-innerHeight);
+  const xAxis = d3.axisBottom(xScale)
+    .tickFormat(xAxisTickFormat)
+    .tickSize(-innerHeight);
+
+  const yAxis = d3.axisLeft(yScale)
+      .tickSize(-innerWidth);
 
   g.append('g')
-    .call(d3.axisLeft(yScale))
+    .call(yAxis)
     .selectAll('.domain')
       .remove();
 
@@ -50,10 +54,10 @@ const render = data => {
     .text('Population');
 
   g.selectAll('rect').data(data)
-    .enter().append('rect')
-      .attr('y', d => yScale(yValue(d)))
-      .attr('width', d => xScale(xValue(d)))
-      .attr('height', yScale.bandwidth());
+    .enter().append('circle')
+      .attr('cy', d => yScale(yValue(d)))
+      .attr('cx', d => xScale(xValue(d)))
+      .attr('r', 18);
 
   g.append('text')
     .attr('class', 'title')
