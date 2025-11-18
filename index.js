96a5aee641eff1126797a50151efd96dfff99fda
elizabethgim <yujingim@naver.com>
2025-11-18 20:14:49 +0900

    create: custom Axes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const height = +svg.attr('height');
 const render = data => {
   const xValue = d => d.population;
   const yValue = d => d.country;
-  const margin = {top:20, right:30, bottom:20, left:100};
+  const margin = {top:50, right:30, bottom:77, left:180};
   const innerWidth = width - margin.left - margin.right;
   const innerHeight = height - margin.top - margin.bottom;
 
@@ -22,15 +22,45 @@ const render = data => {
   const g = svg.append("g")
     .attr('transform', `translate(${margin.left}, ${margin.top})`);
   
-  g.append('g').call(d3.axisLeft(yScale));
-  g.append('g').call(d3.axisBottom(xScale))
+    const xAxisTickFormat = number =>
+      d3.format('.3s')(number)
+      .replace('G', 'B');
+
+    const xAxis = d3.axisBottom(xScale)
+      .tickFormat(xAxisTickFormat)
+      .tickSize(-innerHeight);
+
+  g.append('g')
+    .call(d3.axisLeft(yScale))
+    .selectAll('.domain')
+      .remove();
+
+  const xAxisG = g.append('g').call(xAxis)
     .attr('transform', `translate(0, ${innerHeight})`);
+
+  xAxisG
+    .select('.domain')
+    .remove();
+  
+  xAxisG.append('text')
+    .attr('class', 'axis-label')
+    .attr('y', 65)
+    .attr('x', innerWidth / 2)
+    .attr('fill', 'black')
+    .text('Population');
 
   g.selectAll('rect').data(data)
     .enter().append('rect')
       .attr('y', d => yScale(yValue(d)))
       .attr('width', d => xScale(xValue(d)))
       .attr('height', yScale.bandwidth());
+
+  g.append('text')
+    .attr('class', 'title')
+    .attr('y', -10)
+    .text('Top 10 Most populous Countries');
+  
+  
 };
 
 d3.csv('data.csv').then(data => {
